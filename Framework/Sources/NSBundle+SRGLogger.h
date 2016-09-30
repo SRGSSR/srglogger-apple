86987dac9e37ca8af72375433bb1ebda1c8c407d
Samuel Défago <defagos@gmail.com>
2016-09-30 12:10:50 +0200

    Create framework target

---
+++ Framework/Sources/NSBundle+SRGLogger.h
@@ -0,0 +1,20 @@
+//
+//  Copyright (c) SRG. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSBundle (SRGLogger)
+
+/**
+ *  The logger resource bundle
+ */
++ (NSBundle *)srg_loggerBundle;
+
+@end
+
+NS_ASSUME_NONNULL_END
