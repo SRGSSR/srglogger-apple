ef17bcee1b30f0025d3364060269f9a985d76181
Samuel Défago <defagos@gmail.com>
2016-10-01 09:17:21 +0200

    Add names for all levels

--- Framework/Sources/SRGLogger.m
+++ Framework/Sources/SRGLogger.m
@@ -118,7 +118,10 @@ static SRGLogHandler s_NSLogHandler = ^(NSString *(^message)(void), SRGLogLevel 
     static NSDictionary<NSNumber *, NSString *> *s_levelNames;
     static dispatch_once_t s_onceToken;
     dispatch_once(&s_onceToken, ^{
-        s_levelNames = @{ @(SRGLogLevelWarning) : @"WARN",
+        s_levelNames = @{ @(SRGLogLevelWarning) : @"VERB",
+                          @(SRGLogLevelWarning) : @"DEBUG",
+                          @(SRGLogLevelWarning) : @"INFO",
+                          @(SRGLogLevelWarning) : @"WARN",
                           @(SRGLogLevelError) : @"ERROR" };
     });
     
