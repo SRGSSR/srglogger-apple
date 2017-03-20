6a91a86e125eb08399bd47be22619f5c86cf4142
Samuel Défago <defagos@gmail.com>
2017-03-20 10:48:40 +0100

    Add Swift test case

---
+++ Tests/Sources/LoggerSwiftTestCase.swift
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) SRG. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+import SRGLogger
+import XCTest
+
+class LoggerSwiftTestCase : XCTestCase {
+
+    internal func testLogging() -> Void {
+        
+    }
+}
