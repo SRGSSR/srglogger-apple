b93fe930fb5a2223722673eed697b50494fa0848
Samuel Défago <defagos@gmail.com>
2020-07-18 16:23:52 +0200

    Merge back Swift companion framework

--- Package.swift
+++ Package.swift
@@ -4,18 +4,36 @@ import PackageDescription
 
 let package = Package(
     name: "srglogger-apple",
+    platforms: [
+        .iOS(.v9),
+        .tvOS(.v12),
+        .watchOS(.v5)
+    ],
     products: [
         .library(
             name: "SRGLogger",
             targets: ["SRGLogger"]
+        ),
+        .library(
+            name: "SRGLogger-Swift",
+            targets: ["SRGLogger-Swift"]
         )
     ],
     targets: [
         .target(
             name: "SRGLogger"
         ),
+        .target(
+            name: "SRGLogger-Swift",
+            dependencies: ["SRGLogger"]
+        ),
         .testTarget(
             name: "SRGLogger-tests",
-            dependencies: ["SRGLogger"])
+            dependencies: ["SRGLogger"]
+        ),
+        .testTarget(
+            name: "SRGLogger-Swift-tests",
+            dependencies: ["SRGLogger-Swift"]
+        )
     ]
 )
