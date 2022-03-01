a05d5d961c6bd406cc45eba3b834d75e623b7e8d
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2022-03-01 14:02:06 +0100

    Fix linter issue

--- Sources/SRGLoggerSwift/SRGLogger.swift
+++ Sources/SRGLoggerSwift/SRGLogger.swift
@@ -9,44 +9,44 @@ import SRGLogger
 /**
  *  Log a given message at the verbose level.
  */
-public func SRGLogVerbose(subsystem: String?, category : String?, message: String, file: String = #file, function: String = #function, line: UInt = #line) {
+public func SRGLogVerbose(subsystem: String?, category: String?, message: String, file: String = #file, function: String = #function, line: UInt = #line) {
     SRGLogger.logMessage({ () -> String in
         return message
-    }, level: SRGLogLevel.verbose, subsystem: subsystem, category: category, file: file, function: function, line: line);
+    }, level: SRGLogLevel.verbose, subsystem: subsystem, category: category, file: file, function: function, line: line)
 }
 
 /**
  *  Log a given message at the debug level.
  */
-public func SRGLogDebug(subsystem: String?, category : String?, message: String, file: String = #file, function: String = #function, line: UInt = #line) {
+public func SRGLogDebug(subsystem: String?, category: String?, message: String, file: String = #file, function: String = #function, line: UInt = #line) {
     SRGLogger.logMessage({ () -> String in
         return message
-    }, level: SRGLogLevel.debug, subsystem: subsystem, category: category, file: file, function: function, line: line);
+    }, level: SRGLogLevel.debug, subsystem: subsystem, category: category, file: file, function: function, line: line)
 }
 
 /**
  *  Log a given message at the info level.
  */
-public func SRGLogInfo(subsystem: String?, category : String?, message: String, file: String = #file, function: String = #function, line: UInt = #line) {
+public func SRGLogInfo(subsystem: String?, category: String?, message: String, file: String = #file, function: String = #function, line: UInt = #line) {
     SRGLogger.logMessage({ () -> String in
         return message
-    }, level: SRGLogLevel.info, subsystem: subsystem, category: category, file: file, function: function, line: line);
+    }, level: SRGLogLevel.info, subsystem: subsystem, category: category, file: file, function: function, line: line)
 }
 
 /**
  *  Log a given message at the warning level.
  */
-public func SRGLogWarning(subsystem: String?, category : String?, message: String, file: String = #file, function: String = #function, line: UInt = #line) {
+public func SRGLogWarning(subsystem: String?, category: String?, message: String, file: String = #file, function: String = #function, line: UInt = #line) {
     SRGLogger.logMessage({ () -> String in
         return message
-    }, level: SRGLogLevel.warning, subsystem: subsystem, category: category, file: file, function: function, line: line);
+    }, level: SRGLogLevel.warning, subsystem: subsystem, category: category, file: file, function: function, line: line)
 }
 
 /**
  *  Log a given message at the error level.
  */
-public func SRGLogError(subsystem: String?, category : String?, message: String, file: String = #file, function: String = #function, line: UInt = #line) {
+public func SRGLogError(subsystem: String?, category: String?, message: String, file: String = #file, function: String = #function, line: UInt = #line) {
     SRGLogger.logMessage({ () -> String in
         return message
-    }, level: SRGLogLevel.error, subsystem: subsystem, category: category, file: file, function: function, line: line);
+    }, level: SRGLogLevel.error, subsystem: subsystem, category: category, file: file, function: function, line: line)
 }
